bbcf28ff94cb5b1c867991e5cbe4485e246f1008
lusqua <lucasaguiar.floripa@gmail.com>
2022-03-01 12:50:26 -0300

    Fix. auth test.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  
-  res.status(200).send("início :D");
+const verifyAuth = require("./helpers/authentication_helper");
 
+router.get("/", async (req, res, next) => {
+  const auth = await verifyAuth(req.headers.authorization);
+  if (auth) return res.json("é sobre isso");
+
+  return res.status(401).json();
 });
 
 module.exports = router;
