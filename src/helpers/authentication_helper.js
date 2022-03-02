ef578a8725011476a9f276356aefd6c307989ed6
lusqua <lucasaguiar.floripa@gmail.com>
2022-03-01 23:02:42 -0300

    Fix conditionals and feature returning logged user

--- src/helpers/authentication_helper.js
+++ src/helpers/authentication_helper.js
@@ -2,11 +2,18 @@ const db = require("../../bin/mongodb");
 var ObjectId = require("mongodb").ObjectID;
 
 const verifyAuth = async (Authorization) => {
-  const connections = await db.getDb().collection("Connections");
+  if (!Authorization) return false;
   if (Authorization.length != 24) return false;
+
+  const connections = await db.getDb().collection("Connections");
+  const users = await db.getDb().collection("users");
+
   const auth = await connections.findOne({ _id: ObjectId(Authorization) });
 
-  if (auth) return auth;
+  if (auth) {
+    const user = await users.findOne({ _id: auth.user_id });
+    return user;
+  }
 
   return false;
 };
