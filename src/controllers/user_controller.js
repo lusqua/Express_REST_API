bf753e75b72a3c4b7474ef2eb84fefd7d9e8c866
lusqua <lucasaguiar.floripa@gmail.com>
2022-03-02 15:31:59 -0300

    Feat. user verification to edit/users

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -4,7 +4,12 @@ var ObjectId = require("mongodb").ObjectID;
 const passwordHash = require("password-hash");
 const Database_connection = require("../../bin/mongodb");
 
-router.get("/", (req, res, next) => {
+const verifyAuth = require("../helpers/authentication_helper");
+
+router.get("/", async (req, res, next) => {
+  const { status, err, user } = await verifyAuth(req.headers.authorization);
+  if (status != 200) return res.status(status).json(err);
+
   const users = Database_connection.getDb().collection("users");
 
   users
@@ -19,7 +24,10 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.get("/:username", (req, res, next) => {
+router.get("/:username", async (req, res, next) => {
+  const { status, err, user } = await verifyAuth(req.headers.authorization);
+  if (status != 200) return res.status(status).json(err);
+
   const users = Database_connection.getDb().collection("users");
 
   users.findOne({ username: req.params.username }, (err, usr) => {
@@ -28,20 +36,28 @@ router.get("/:username", (req, res, next) => {
   });
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+  const { status, err, user } = await verifyAuth(req.headers.authorization);
+  if (status != 200) return res.status(status).json(err);
+
   const users = Database_connection.getDb().collection("users");
 
-  user = {
+  new_user = {
     username: req.body.username,
-    password: passwordHash.generate(req.body.password),
+    password: passwordHash.generate(
+      req.body.password.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+    ),
     created_at: new Date().toJSON(),
+    created_by: user.username,
   };
+
+  new_user.access_level = req.body.acess_level || 0;
 
   users.findOne({ username: req.body.username }, (err, usr) => {
     if (usr) {
       res.json("Usuário já existe");
     } else {
-      users.insertOne(user, (err, result) => {
+      users.insertOne(new_user, (err, result) => {
         if (err) {
           res.status(400).send("Something failed.");
         } else {
@@ -52,11 +68,15 @@ router.post("/", (req, res, next) => {
   });
 });
 
-router.put("/:username", (req, res, next) => {
+router.put("/:username", async (req, res, next) => {
+  const { status, err, user } = await verifyAuth(req.headers.authorization);
+  if (status != 200) return res.status(status).json(err);
+
   const users = Database_connection.getDb().collection("users");
 
   edited_user = {
     updated_at: new Date().toJSON(),
+    updated_by: user.username,
   };
 
   if (req.body.new_username) edited_user["username"] = req.body.new_username;
@@ -65,6 +85,8 @@ router.put("/:username", (req, res, next) => {
 
   users.findOne({ username: req.params.username }, (err, usr) => {
     if (!usr) return res.status(404).json("User not found.");
+    if (user.access_level == 0 && usr != user)
+      return res.status(401).json("You can't edited others users");
     if (!passwordHash.verify(req.body.password, usr.password))
       return res.status(403).json("Wrong password.");
     if (passwordHash.verify(req.body.password, usr.password))
