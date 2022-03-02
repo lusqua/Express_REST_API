e739b3a3278ead30c197bc5e38a7d4854cbfba6a
lusqua <lucasaguiar.floripa@gmail.com>
2022-03-01 23:02:00 -0300

    fix. post security logic

--- src/controllers/posts_controller.js
+++ src/controllers/posts_controller.js
@@ -3,6 +3,8 @@ const router = require("express").Router();
 const db = require("../../bin/mongodb");
 var ObjectId = require("mongodb").ObjectID;
 
+const verifyAuth = require("../helpers/authentication_helper");
+
 router.get("/", (req, res, next) => {
   const dbConnect = db.getDb();
 
@@ -19,11 +21,15 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+  const user = await verifyAuth(req.headers.authorization);
+  if (!user) return res.status(401).json();
+
   const dbConnect = db.getDb();
 
   post = {
-    author: req.body.author,
+    author: user.username,
+    author_id: user._id,
     created_at: new Date().toJSON(),
     content: req.body.content,
   };
@@ -32,42 +38,45 @@ router.post("/", (req, res, next) => {
     if (err) {
       res.status(400).send("Erro em criar post");
     } else {
-      res.status(204).send();
+      res.status(200).json(post);
     }
   });
 });
 
-router.put("/:id", (req, res, next) => {
-  const dbConnect = db.getDb();
+router.put("/:id", async (req, res, next) => {
+  if (!req.params.id || req.params.id.length != 24)
+    return res.status(404).json();
+  if (!req.body.content) return res.status(204).json();
 
-  post = {
+  const user = await verifyAuth(req.headers.authorization);
+  if (!user) return res.status(401).json();
+
+  const posts = db.getDb().collection("posts");
+
+  edited_post = {
+    content: req.body.content,
     updated_at: new Date().toJSON(),
   };
-  if (typeof req.body.author != "undefined") {
-    post["author"] = req.body.author;
-  }
-  if (typeof req.body.content != "undefined") {
-    post["content"] = req.body.content;
-  }
 
-  dbConnect
-    .collection("posts")
-    .updateOne(
-      { _id: ObjectId(req.params.id) },
-      { $set: post },
-      function (err, result) {
-        if (err) {
-          res.status(400).send("Erro ao encontrar o id");
-        } else {
-          dbConnect
-            .collection("posts")
-            .find({ _id: ObjectId(req.params.id) })
-            .limit(1)
-            .toArray((err, result) => {
-              res.status(200).json(result);
-            });
-        }
+  const post = await posts.findOne({ _id: ObjectId(req.params.id) });
+
+  if (post.author_id.str != user._id.str) return res.status(401).json();
+
+  posts.updateOne(
+    { _id: ObjectId(req.params.id) },
+    { $set: edited_post },
+    function (err, result) {
+      if (err) {
+        res.status(400).json("Error trying find ID");
+      } else {
+        posts
+          .find({ _id: ObjectId(req.params.id) })
+          .limit(1)
+          .toArray((err, result) => {
+            res.status(200).json(result);
+          });
       }
-    );
+    }
+  );
 });
 module.exports = router;
